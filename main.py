b8a230b36175a889977645c14f5fe64c7edbf831
Diego Fioravanti <fioravanti.diego@gmail.com>
2018-09-05 14:05:51 +0200

    Added parser

---
+++ main.py
@@ -0,0 +1,60 @@
+import argparse
+import random
+
+import torch
+
+# Training settings
+parser = argparse.ArgumentParser(description='VAE')
+
+# arguments for optimization
+parser.add_argument('--batch_size', type=int, default=100, metavar='BStrain',
+                    help='input batch size for training (default: 100)')
+parser.add_argument('--test_batch_size', type=int, default=100, metavar='BStest',
+                    help='input batch size for testing (default: 100)')
+parser.add_argument('--epochs', type=int, default=2000, metavar='E',
+                    help='number of epochs to train (default: 2000)')
+parser.add_argument('--lr', type=float, default=0.0005, metavar='LR',
+                    help='learning rate (default: 0.0005)')
+parser.add_argument('--early_stopping_epochs', type=int, default=50, metavar='ES',
+                    help='number of epochs for early stopping')
+
+parser.add_argument('--warmup', type=int, default=100, metavar='WU',
+                    help='number of epochs for warmu-up')
+
+# cuda
+parser.add_argument('--no-cuda', action='store_true', default=True,
+                    help='enables CUDA training')
+
+# random seed
+parser.add_argument('--seed', type=int, default=42, metavar='S',
+                    help='random seed (default: 42)')
+
+# model
+parser.add_argument('--z_size', type=int, default=40, metavar='M1',
+                    help='latent space size (default: 40)')
+
+parser.add_argument('--prior', type=str, default='standard', metavar='P',
+                    help='prior: standard, gaussian_copula')
+
+# experiment
+parser.add_argument('--S', type=int, default=5000, metavar='SLL',
+                    help='number of samples used for approximating log-likelihood')
+parser.add_argument('--MB', type=int, default=100, metavar='MBLL',
+                    help='size of a mini-batch used for approximating log-likelihood')
+
+# dataset
+parser.add_argument('--dataset_name', type=str, default='binary_mnist', metavar='DN',
+                    help='name of the dataset: binary_mnist, dynamic_mnist,'
+                         ' omniglot, caltech101silhouettes, histopathologyGray, freyfaces, cifar10')
+
+parser.add_argument('--dynamic_binarization', action='store_true', default=False,
+                    help='allow dynamic binarization')
+
+
+args = parser.parse_args()
+args.cuda = args.no_cuda and torch.cuda.is_available()
+
+random.seed(args.seed)
+torch.manual_seed(args.seed)
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)

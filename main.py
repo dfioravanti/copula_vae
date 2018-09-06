bfb34239e7f2c06d3000111ac2c00197ba97fd5b
Diego Fioravanti <fioravanti.diego@gmail.com>
2018-09-06 17:30:22 +0200

    Added main loop

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import argparse
 import random
 
+from models import VAE
+from utils import datasets
+
 import torch
+from torch.utils.data import DataLoader
+from torch.optim import Adam
 
 # Training settings
 parser = argparse.ArgumentParser(description='VAE')
@@ -50,7 +55,6 @@ parser.add_argument('--dataset_name', type=str, default='binary_mnist', metavar=
 parser.add_argument('--dynamic_binarization', action='store_true', default=False,
                     help='allow dynamic binarization')
 
-
 args = parser.parse_args()
 args.cuda = args.no_cuda and torch.cuda.is_available()
 
@@ -58,3 +62,44 @@ random.seed(args.seed)
 torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
+
+
+def main(args):
+
+    train_loader, validation_loader, test_loader = load_dataset(args.dataset_name)
+
+    if args.prior == 'standard':
+        model = VAE()
+
+    model.train_dataset(train_loader,
+                        validation_loader,
+                        optimizer=Adam(),
+                        epochs=50,
+                        warmup=10,
+                        verbose=True,
+                        early_stopping_tolerance=10)
+
+
+def load_dataset(dataset_name, batch_size=50):
+    if dataset_name == 'binary_mnist':
+        train_dataset = datasets.BinaryMNISTDataset('datasets/', True, train=True)
+        test_dataset = datasets.BinaryMNISTDataset('datasets/', True, train=False, test=True)
+        val_dataset = datasets.BinaryMNISTDataset('datasets/', True, train=True, validation=True)
+
+    train_loader = DataLoader(train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True, num_workers=4)
+
+    validation_loader = DataLoader(val_dataset,
+                                   batch_size=batch_size,
+                                   shuffle=True, num_workers=4)
+
+    test_loader = DataLoader(test_dataset,
+                             batch_size=batch_size,
+                             shuffle=True, num_workers=4)
+
+    return train_loader, validation_loader, test_loader
+
+
+if __name__ == '__main__':
+    main(args)

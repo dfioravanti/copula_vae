Refactored + fixed covariance matrix + Broken marginalVAE

models/BaseCopulaVAE.py
@@ -4,7 +4,7 @@ import torch
 from models.BaseVAE import BaseVAE
 from utils.copula_sampling import sampling_from_gausiann_copula
 from utils.distributions import log_density_bernoulli, log_density_discretized_Logistic
-
+from utils.settings import tollerance
 
 class BaseCopulaVAE(BaseVAE):
 
@@ -21,6 +21,9 @@ class BaseCopulaVAE(BaseVAE):
 
         self.number_neurons_L = dimension_latent_space * (dimension_latent_space + 1) // 2
 
+        stuff = np.expand_dims(np.expand_dims(np.arange(1, self.dimension_latent_space + 1), axis=0), axis=-1)
+        self.stuff = torch.tensor(stuff).float().to(self.device)
+
     def forward(self, x):
 
         s_x, L_x = self.q_s(x)
@@ -35,7 +38,7 @@ class BaseCopulaVAE(BaseVAE):
         batch_size = x.shape[0]
         L_x = self.compute_L_x(x, batch_size)
 
-        s = sampling_from_gausiann_copula(L_x, batch_size, self.dimension_latent_space)
+        s = sampling_from_gausiann_copula(L_x, self.dimension_latent_space, batch_size)
 
         return s, L_x
 
@@ -53,13 +56,14 @@ class BaseCopulaVAE(BaseVAE):
 
         idx_trn = np.tril_indices(self.dimension_latent_space)
         idx_diag = np.diag_indices(self.dimension_latent_space)
-        idx_not_diag = np.tril_indices(self.dimension_latent_space, -1)
 
         L_x = torch.zeros([batch_size, self.dimension_latent_space, self.dimension_latent_space]).to(self.device)
 
-        L_x[:, idx_trn[0], idx_trn[1]] = self.L_layers(x)
-        L_x[:, idx_diag[0], idx_diag[1]] = torch.sigmoid(L_x[:, idx_diag[0], idx_diag[1]]) + 1e-5
-        L_x[:, idx_not_diag[0], idx_not_diag[1]] = L_x[:, idx_not_diag[0], idx_not_diag[1]]
+        L_x[:, idx_trn[0], idx_trn[1]] = torch.sigmoid(self.L_layers(x))
+        L_x[:, idx_diag[0], idx_diag[1]] = torch.tanh(L_x[:, idx_diag[0], idx_diag[1]])
+        L_x += tollerance
+
+        L_x /= self.stuff
 
         return L_x
 
@@ -127,7 +131,6 @@ class BaseDiagonalCopulaVAE(BaseCopulaVAE):
                  input_shape,
                  dataset_type,
                  device=torch.device("cpu")):
-
         super(BaseDiagonalCopulaVAE, self).__init__(dimension_latent_space=dimension_latent_space,
                                                     input_shape=input_shape,
                                                     dataset_type=dataset_type,
@@ -136,9 +139,8 @@ class BaseDiagonalCopulaVAE(BaseCopulaVAE):
         self.number_neurons_L = dimension_latent_space
 
     def compute_L_x(self, x, batch_size):
-
         idx_diag = np.diag_indices(self.dimension_latent_space)
         L_x = torch.zeros([batch_size, self.dimension_latent_space, self.dimension_latent_space]).to(self.device)
-        L_x[:, idx_diag[0], idx_diag[1]] = torch.sigmoid(self.L_layers(x)) + 1e-7
+        L_x[:, idx_diag[0], idx_diag[1]] = self.L_layers(x)
 
         return L_x

models/ShallowLayer.py
@@ -134,7 +134,6 @@ class ShallowDiagonalMarginalVAE(BaseDiagonalCopulaVAE):
                  dimension_latent_space,
                  input_shape,
                  dataset_type,
-                 encoder_output_size=300,
                  marginals='gaussian',
                  device=torch.device("cpu")):
 
@@ -146,14 +145,14 @@ class ShallowDiagonalMarginalVAE(BaseDiagonalCopulaVAE):
         self.marginals = marginals
 
         # Encoder q(s | x)
-        # L(x) without the final activation function
 
         self.L_layers = nn.Sequential(
             nn.Linear(np.prod(self.input_shape), 300),
             nn.Tanh(),
             nn.Linear(300, 300),
             nn.Tanh(),
-            nn.Linear(300, self.number_neurons_L)
+            nn.Linear(300, self.number_neurons_L),
+            nn.Sigmoid()
         )
 
         # Decoder p(x|s)

utils/copula_sampling.py
@@ -1,14 +1,18 @@
+"""
+    This file contains all the functions used to compute copulas and related auxiliary functions
+"""
+
 import numpy as np
 from numpy.random import uniform
 from scipy.stats import levy_stable
-from scipy import stats
 
 import torch
+
 from utils.inverse_distibutions import standard_normal_cdf
+from utils.settings import tollerance
 
 
 def partition(number, classes):
-
     sizes = np.zeros(classes, dtype=np.int)
 
     sizes[:] = number // classes
@@ -24,7 +28,7 @@ def psi_theta(psi, theta):
 
 
 def psi_gumbel(t, theta):
-    return np.exp(-t ** (1/theta))
+    return np.exp(-t ** (1 / theta))
 
 
 def marshall_olkin_sampling(psi_theta, F, d):
@@ -35,20 +39,19 @@ def marshall_olkin_sampling(psi_theta, F, d):
 
 
 def sampling_from_gumbel_copula(theta, d):
-    F = levy_stable(1/theta, 1, 0, (np.cos(np.pi/(2*theta)) ** theta))
+    F = levy_stable(1 / theta, 1, 0, (np.cos(np.pi / (2 * theta)) ** theta))
     psi = psi_theta(psi_gumbel, theta)
     return marshall_olkin_sampling(psi, F, d)
 
 
 def sampling_from_gumbel_partially_nested_copula(d, s, thetas):
-
     if not np.all(thetas >= 1):
         raise ValueError("All the thetas must be bigger than one")
 
     if not np.all(np.diff(thetas) >= 0):
         raise ValueError("Thetas must be increasing")
 
-    if not len(thetas) == s+1:
+    if not len(thetas) == s + 1:
         raise ValueError("Length of thetas must be equas to s+1")
 
     sizes = partition(d, s)
@@ -60,73 +63,85 @@ def sampling_from_gumbel_partially_nested_copula(d, s, thetas):
 
     used_elemets = 0
 
-    for i in range(1, s+1):
-
-        future_used_elements = used_elemets + sizes[i-1]
+    for i in range(1, s + 1):
+        future_used_elements = used_elemets + sizes[i - 1]
 
         theta_i = thetas[i] / thetas[0]
-        xs[used_elemets:future_used_elements] = sampling_from_gumbel_copula(theta_i, sizes[i-1])
+        xs[used_elemets:future_used_elements] = sampling_from_gumbel_copula(theta_i, sizes[i - 1])
 
         used_elemets = future_used_elements
 
     return psi_0(-np.log(xs) / v_0)
 
 
-def sampling_from_gausiann_copula(L, batch_size, n):
+def sampling_from_gausiann_copula(L, d, n=1):
+    """
+    Samples (n, d) elements from a gaussian copula with correlation matrix R = LL^t
 
-    size = (batch_size, 1, n)
+    For one sample the algorithm used is the following
+        1) Decompose R with Cholesky (we assume that the decomposition is computed outside)
+        2) a) Generate a vector Z = (Z_1,...,Z_d)^t of independent standard normal variates
+           b) X = LZ
+           c) return (Phi(X_1), ..., Phi(X_d))
 
-    es = torch.randn(size=size)
+    more info https://stats.stackexchange.com/questions/37424/how-to-simulate-from-a-gaussian-copula
+
+    Parameters
+    ----------
+    L: FloatTensor
+        Positively defined lower triangular matrix obtained by the Cholesky decomposition of our
+        desired correlation matrix R
+    d: int
+        Dimension of [0,1]^d where we are sampling the copula
+    n: int
+        Number of samples to be samples
+
+    Returns
+    -------
+    FloatTensor tuple
+        (n, d) elements coming from the copula
+    """
+
+    size = (n, 1, d)
+    L = torch.clamp(L, min=0 + tollerance, max=1 - tollerance)
+
+    zs = torch.randn(size=size)
     if L.is_cuda:
-        es = es.to(L.get_device())
+        zs = zs.to(L.get_device())
 
-    xs = (es @ L).view(batch_size, -1)
+    xs = (zs @ L).view(n, -1)
 
     return standard_normal_cdf(xs)
 
 
 if __name__ == '__main__':
-
     import matplotlib
 
-    matplotlib.use('TkAgg')
+    matplotlib.use('Qt5Agg')
+    import matplotlib.pyplot as plt
     import seaborn as sns
     import pandas as pd
-
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
+    from utils.inverse_distibutions import normal_icdf
 
     np.random.seed(123)
-    n = 20000
+    d = 3
+    n = 60000
 
-    cov = np.matrix([[1, 0, 0],
-                    [0, 1, 0],
-                    [0, 0, 1]]
-                    )
+    L = np.matrix([[1, 0, 0],
+                   [0, 1, 0],
+                   [0.5, 0, 1]]
+                  )
+    print(L @ L.T)
+    L = torch.tensor(L).float()
 
-    #cov = np.eye(3)
-
-    xs = sampling_from_gausiann_copula(cov, n)
-    zs = np.zeros_like(xs)
+    xs = sampling_from_gausiann_copula(L, d, n)
+    zs = normal_icdf(xs)
 
     fig = plt.figure()
-    #ax = fig.gca(projection='3d')
-
-    f = g = h = stats.norm(0, 1)
-#    g = stats.poisson(1)
- #   h = stats.bernoulli(0.5)
-
-    zs[:, 0], zs[:, 1], zs[:, 2] = f.ppf(xs[:, 0]), g.ppf(xs[:, 1]), h.ppf(xs[:, 2])
-
-    #plt.scatter(zs[:, 0], zs[:, 1], color='blue')
-    #ts[:, 0], ts[:, 1], ts[:, 2] = f.ppf(xs[:, 0]), g.ppf(xs[:, 1]), h.ppf(xs[:, 2])
-
-    ys = np.random.normal(size=(n, 2))
-    #plt.scatter(ys[:, 0], ys[:, 1], color='red')
-
-    #
-
-    df = pd.DataFrame(zs)
-    sns.distplot(ys[:, 0])
     sns.distplot(zs[:, 0])
+    sns.distplot(zs[:, 1])
+    sns.distplot(zs[:, 2])
+    plt.show()
+    zs = pd.DataFrame(zs.numpy())
+    sns.pairplot(zs)
     plt.show()

utils/inverse_distibutions.py
@@ -6,10 +6,7 @@ import math
 
 import torch
 
-from utils.distributions import tollerance
-
-# We need this to avoid numerical instability
-tollerance = 1e-7
+from utils.settings import tollerance
 
 
 # CDF
@@ -53,7 +50,7 @@ def standard_normal_icdf(p):
     return torch.erfinv(2 * p - 1) * math.sqrt(2)
 
 
-def gaussian_icdf(p, loc, scale):
+def normal_icdf(p, loc=0, scale=1):
     """
     Compute the inverse cdf of a gaussian with mean as loc and variance as scale.
 

utils/nn.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Parameter
 
-from utils.inverse_distibutions import gaussian_icdf, laplace_icdf, cauchy_icdf, exp_icdf, logNorm_icdf
+from utils.inverse_distibutions import normal_icdf, laplace_icdf, cauchy_icdf, exp_icdf, logNorm_icdf
 
 
 class Flatten(nn.Module):
@@ -64,7 +64,7 @@ class ICDF(nn.Module):
         # log(exp(x) + 1) is used to force the output to be positive
 
         if self.distribution == 'gaussian':
-            return gaussian_icdf(p=x, loc=self.loc, scale=torch.log(torch.exp(self.scale) + 1))
+            return normal_icdf(p=x, loc=self.loc, scale=torch.log(torch.exp(self.scale) + 1))
         elif self.distribution == 'laplace':
             return laplace_icdf(p=x, loc=self.loc, scale=torch.log(torch.exp(self.scale) + 1))
         elif self.distribution == 'log_norm':

utils/training.py
@@ -23,10 +23,6 @@ def train_epoch(model, epoch, loader, optimizer, beta, rec_img_path=None, writer
 
         optimizer.zero_grad()
 
-        # TODO: move reshape inside the model.
-        #       So if for some reason I decide to start with CNN it still works.
-        #       Inputs and outputs should always be  of the original shape
-
         xs = xs.view(loader.batch_size, -1).to(device)
 
         if batch_idx == 0 and writer is not None:
@@ -81,17 +77,14 @@ def validation_epoch(model, beta, loader, device=torch.device("cpu")):
 
 
 def compute_beta(epoch, warmup):
-    if warmup is None:
-        beta = 1
-    else:
-        beta = min(epoch / warmup, 1)
 
-    return beta
+    return min(epoch / warmup, 1) if warmup is not None else 1
 
 
 def train_on_dataset(model, loader_train, loader_validation, optimizer, epochs=50, warmup=None, grace_early_stopping=10,
                      checkpoint=None, output_dir=None, frequency_checkpoints=None, checkpoint_dir=None, writer=None,
                      device=torch.device("cpu")):
+    # Initialization
     best_loss = math.inf
     early_stopping_strikes = 0
     starting_epoch = 0
@@ -101,11 +94,10 @@ def train_on_dataset(model, loader_train, loader_validation, optimizer, epochs=5
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
         starting_epoch = checkpoint['epoch']
 
-    model.train()
-
     rec_img_path = output_dir / 'rec_img'
     rec_img_path.mkdir(parents=True, exist_ok=True)
 
+    # Main loop
     for epoch in range(starting_epoch, epochs):
 
         beta = compute_beta(epoch, warmup)